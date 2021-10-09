003541ee57e55919ffb85a4b0a4d2dbeb576aaec
nataly tuardy <nattaurdy@gmail.com>
2021-10-09 10:40:15 +0300

    finished homework3

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import { Component } from "react";
+import "./App.css";
+import ContactForm from "./components/contactForm/ContactForm";
+import ContactList from "./components/contactList/ContactList";
+import Filter from "./components/filter/Filter";
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: "",
+  };
+
+  componentDidMount() {
+    const contactsString = localStorage.getItem("contacts");
+    const contactsParse = JSON.parse(contactsString);
+    if (contactsParse) {
+      this.setState({ contacts: contactsParse });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
+
+  handleFilter = () => {
+    const { filter, contacts } = this.state;
+    const filteredContacts = contacts.filter((item) =>
+      item.name.toLowerCase().includes(filter.toLowerCase())
+    );
+    return filteredContacts;
+  };
+
+  handleChange = ({ target }) => {
+    const { value, name } = target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (newContact) => {
+    this.setState(({ contacts }) => {
+      const findDoubleContact = contacts.find(
+        (contact) => contact.name === newContact.name
+      );
+      if (findDoubleContact) {
+        alert(`${findDoubleContact.name} is already in contacts`);
+        return;
+      }
+      const newContactsList = [...contacts, newContact];
+      return {
+        contacts: newContactsList,
+        name: "",
+        number: "",
+      };
+    });
+  };
+
+  handleDelete = (id) => {
+    this.setState(({ contacts }) => {
+      const copyContactsList = [...contacts];
+      const newContactsList = copyContactsList.filter(
+        (contact) => contact.id !== id
+      );
+      return {
+        contacts: newContactsList,
+      };
+    });
+  };
+
+  render() {
+    const { handleChange, handleSubmit, handleFilter, handleDelete } = this;
+    const { contacts, filter } = this.state;
+    return (
+      <div className="App">
+        <h1 className="App-header">Phonebook</h1>
+        <ContactForm onSubmit={handleSubmit} />
+        <Filter onChange={handleChange} data={contacts} filter={filter} />
+        <ContactList data={handleFilter()} onDelete={handleDelete} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/contactForm/ContactForm.jsx
@@ -0,0 +1,46 @@
+import { Component } from "react";
+import { generate } from "shortid";
+import InputField from "../inputField/InputField";
+import {list} from "../../fields/fields"
+
+class ContactForm extends Component {
+  state = {
+    name: "",
+    number: "",
+  };
+
+  handleChange = ({ target }) => {
+    const { value, name } = target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const id = generate();
+    const { name, number } = this.state;
+    const newContact = { id, name, number };
+    this.props.onSubmit(newContact);
+    this.setState({
+      name: "",
+      number: "",
+    });
+  };
+
+  render() {
+    const { name, number } = this.state;
+    return (
+      <>
+        <form onSubmit={this.handleSubmit} className="form-control mb-3">
+          <InputField {...list.name} value={name} onChange={this.handleChange}/>
+          <InputField {...list.number} value={number} onChange={this.handleChange}/>
+          <div class="col-12">
+            <button type="submit" className="btn btn-primary">
+            Add contact
+            </button>
+          </div>
+        </form>
+      </>
+    );
+  }
+}
+export default ContactForm;
